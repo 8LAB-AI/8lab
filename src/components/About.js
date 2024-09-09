374df37afc0fe4183c231a0aab0504cc33936644
Max Gonzih <gonzih@gmail.com>
2024-09-09 16:27:00 -0700

    Bunch of tweaks

--- src/components/About.js
+++ src/components/About.js
@@ -12,7 +12,7 @@ export default function About(){
                 <div className='relative z-[3] w-full [text-shadow:_0.5px_0.5px_0_#00c01d]'>
                 
                 <div className="relative h-full bg-black z-[3]">
-                <div className="px-[45px] flex flex-col-reverse lg:flex-row gap-[30px] lg:gap-[100px] py-[150px] relative z-[3]">
+                <div className="px-[45px] flex flex-col-reverse lg:flex-row gap-[30px] lg:gap-[100px] py-[50px] relative z-[3]">
                 
                 <div className='lg:flex-1 text-center'>
                     <h2 className="font-kl lg:text-2xl text-mobile2xl mb-[5px] uppercase">
@@ -21,12 +21,13 @@ export default function About(){
                     <span className="block lg:text-lg text-mobilebase mb-[10px]">
                         Want to know? Apply below.
                     </span>
+                    <span className="block text-9xl"> ↓ </span>
                 </div>
                 </div>
             </div>
                 </div>
                 <div class="vfrc-footer--watermark"></div>
-            <div id="chatTarget" style={{zIndex: 2, position: 'relative', textShadow: 'none'}}></div>
+            <div id="chatTarget" style={{zIndex: 200, position: 'relative', textShadow: 'none'}}></div>
 
                 
             </Element>
