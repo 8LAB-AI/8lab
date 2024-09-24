09b530afdac9fd4bb4ff95c05f7f75bbd2b69c79
Max Gonzih <gonzih@gmail.com>
2024-09-23 18:19:00 -0700

    Kek

--- src/components/Info.js
+++ src/components/Info.js
@@ -43,17 +43,17 @@ export default function Info(){
                         <div className="flex-1 mb-[100px] lg:mb-0">
                             <Image className="lg:mb-[20px] mb-[10px] h-[50px] lg:h-auto w-auto" src={Achieve} height={40} alt="oops" />
                             <h3 className="text-xl mb-[20px] font-kl uppercase">Find</h3>
-                            <span className="lg:text-lg text-mobilelg block mb-[15px]">Discover curated connections—people, brands, and opportunities tailored to your vision. Earn points as you build meaningful relationships and influence.</span>
+                            <span className="lg:text-lg text-mobilelg block mb-[15px]">New connections: people, brands, and opportunities aligned with your vision. Score points as you unlock meaningful relationships and amplify your network.</span>
                         </div>
                         <div className="flex-1 mb-[100px] lg:mb-0">
                             <Image className="lg:mb-[20px] mb-[10px] h-[50px] lg:h-auto w-auto" src={Innovate} height={40} alt="oops" />
                             <h3 className="text-xl mb-[20px] font-kl uppercase">Be found</h3>
-                            <span className="lg:text-lg text-mobilelg block mb-[15px]">Elevate your profile and get seen by the right brands and collaborators. Gain badges like &quot;Dot Connector&quot; or &quot;Creative Innovator&quot; as you actively contribute to the community.</span>
+                            <span className="lg:text-lg text-mobilelg block mb-[15px]">Elevate your profile and get seen by the right brands and collaborators. Earn badges like &quot;Dot Connector&quot; or &quot;Creative Innovator&quot; as you actively contribute to the community.</span>
                         </div>
                         <div className="flex-1 mb-[100px] lg:mb-0">
                             <Image className="lg:mb-[20px] mb-[10px] h-[50px] lg:h-auto w-auto" src={Impact} height={40} alt="oops" />
                             <h3 className="text-xl mb-[20px] font-kl uppercase">Grow</h3>
-                            <span className="lg:text-lg text-mobilelg block mb-[15px]">Level up your skills through engaging challenges and quests. Get hands-on support to expand your abilities and boost your income. Unlock new tools, perks, and exclusive rewards as you progress on your  journey.</span>
+                            <span className="lg:text-lg text-mobilelg block mb-[15px]">Expand your skills and income through real-world projects. Get support to scale your creative practice, unlock resources, and new opportunities as you progress.</span>
                         </div>
                         <div className="flex-1 mb-[100px] lg:mb-0">
                             <Image className="lg:mb-[20px] mb-[10px] h-[50px] lg:h-auto w-auto" src={Money} height={40} alt="oops" />
